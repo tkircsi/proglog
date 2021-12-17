40ffca15f8b4444f45831b104fea4aa5bdb08677
Tibor Kircsi <tkircsi@gmail.com>
2021-12-17 14:49:26 +0100

    add server TLS credentials

--- internal/server/server_test.go
+++ internal/server/server_test.go
@@ -8,9 +8,10 @@ import (
 
 	"github.com/stretchr/testify/require"
 	api "github.com/tkircsi/proglog/api/v1"
+	"github.com/tkircsi/proglog/internal/config"
 	"github.com/tkircsi/proglog/internal/log"
 	"google.golang.org/grpc"
-	"google.golang.org/grpc/credentials/insecure"
+	"google.golang.org/grpc/credentials"
 	"google.golang.org/grpc/status"
 )
 
@@ -34,13 +35,35 @@ func TestServer(t *testing.T) {
 func setupTest(t *testing.T, fn func(*Config)) (client api.LogClient, cfg *Config, teardown func()) {
 	t.Helper()
 
-	l, err := net.Listen("tcp", ":0")
+	l, err := net.Listen("tcp", "127.0.0.1:0")
 	require.NoError(t, err)
 
+	// Client setup without TLS
 	//clientOptions := []grpc.DialOption{grpc.WithInsecure()}
-	clientOptions := []grpc.DialOption{grpc.WithTransportCredentials(insecure.NewCredentials())}
+	// clientOptions := []grpc.DialOption{grpc.WithTransportCredentials(insecure.NewCredentials())}
+
+	// Client setup with TLS
+	clientTLSConfig, err := config.SetupTLSConfig(config.TLSConfig{
+		CAFile: config.CAFile,
+	})
+	require.NoError(t, err)
+
+	clientCreds := credentials.NewTLS(clientTLSConfig)
+	clientOptions := []grpc.DialOption{grpc.WithTransportCredentials(clientCreds)}
+
 	cc, err := grpc.Dial(l.Addr().String(), clientOptions...)
 	require.NoError(t, err)
+	client = api.NewLogClient(cc)
+
+	// Server setup with TLS
+	serverTLSConfig, err := config.SetupTLSConfig(config.TLSConfig{
+		CertFile:      config.ServerCertFile,
+		KeyFile:       config.ServerKeyFile,
+		CAFile:        config.CAFile,
+		ServerAddress: l.Addr().String(),
+	})
+	require.NoError(t, err)
+	serverCreds := credentials.NewTLS(serverTLSConfig)
 
 	dir, err := ioutil.TempDir("", "server-test")
 	require.NoError(t, err)
@@ -56,14 +79,12 @@ func setupTest(t *testing.T, fn func(*Config)) (client api.LogClient, cfg *Confi
 		fn(cfg)
 	}
 
-	server, err := NewGRPCServer(cfg)
+	server, err := NewGRPCServer(cfg, grpc.Creds(serverCreds))
 	require.NoError(t, err)
 
 	go func() {
 		server.Serve(l)
 	}()
-
-	client = api.NewLogClient(cc)
 
 	return client, cfg, func() {
 		server.Stop()
